33d166c9745aeadc372fc6d503b1189da6c5528a
JoeMaxwell <JoeMaxwell@Noir>
2022-11-09 20:00:18 +0700

    menambahkan "final" pada setiap variabel yang ada interface TipePengiriman

--- src/Model/TipePengiriman.java
+++ src/Model/TipePengiriman.java
@@ -2,8 +2,8 @@ package Model;
 
 public interface TipePengiriman {
 
-    int JNE = 0;
-    int GOJEK = 1;
-    int JNT = 2;
-    int PEGAWAI = 3;
+    final int JNE = 0;
+    final int GOJEK = 1;
+    final int JNT = 2;
+    final int PEGAWAI = 3;
 }
