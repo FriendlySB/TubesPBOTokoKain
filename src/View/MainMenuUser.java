6496ea464bfc56941b6106a0b5bca91382c23fb8
jericho <limtu@DESKTOP-TG1L28O>
2022-11-12 13:56:48 +0700

    menambahkan menuLihatProfile

--- src/View/MainMenuUser.java
+++ src/View/MainMenuUser.java
@@ -13,46 +13,48 @@ import javax.swing.*;
 import Model.Customer;
 import javax.swing.filechooser.FileNameExtensionFilter;
 
-public class MainMenuUser{
-    
+public class MainMenuUser {
+
     public MainMenuUser(Customer customer) {
         JFrame frame = new JFrame();
-        frame.setSize(600,400);
+        frame.setSize(600, 400);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLocationRelativeTo(null);
         frame.setLayout(null);
         frame.setVisible(true);
         frame.setTitle("Main Menu User");
-        
+
         JPanel panel = new JPanel();
         panel.setSize(600, 400);
         panel.setLayout(null);
         panel.setVisible(true);
-        
-        JLabel judul = new JLabel("Selamat Datang di Toko Kain, Anonymous!");
+
+        JLabel judul = new JLabel("Selamat Datang di Toko Kain, "+ customer.getUsername());
         judul.setBounds(180, 20, 300, 50);
-        
+
         JButton menuBeliKain = new JButton("Beli Kain");
         menuBeliKain.setBounds(150, 100, 300, 50);
         menuBeliKain.addActionListener(e -> {
-            frame.dispose();
             new MenuBeliKain();
         });
-        
+
         JButton menuCekTransaksi = new JButton("Transaksi");
         menuCekTransaksi.setBounds(150, 160, 300, 50);
-        
+
         JButton menuLihatProfile = new JButton("Profile");
         menuLihatProfile.setBounds(150, 220, 300, 50);
-        
+        menuLihatProfile.addActionListener(e -> {
+            new MenuLihatProfile(customer);
+        });
+
         JButton menuLogout = new JButton("Log Out");
         menuLogout.setBounds(150, 280, 300, 50);
-        
-        JMenuBar mb = new JMenuBar();  
+
+        JMenuBar mb = new JMenuBar();
         JButton menuCart = new JButton("Cart");
         mb.add(Box.createGlue());
         mb.add(menuCart);
-        
+
         panel.add(judul);
         panel.add(menuBeliKain);
         panel.add(menuCekTransaksi);
@@ -61,5 +63,5 @@ public class MainMenuUser{
         frame.setJMenuBar(mb);
         frame.add(panel);
     }
-    
+
 }
