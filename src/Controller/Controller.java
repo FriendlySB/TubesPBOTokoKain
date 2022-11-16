e3b7d49ff3408f802a6c92c829d4d60c97660794
FriendlySB <USER@LAPTOP-AND1GB1A>
2022-11-16 20:19:38 +0700

    Tambah controller get nama by id_kain

--- src/Controller/Controller.java
+++ src/Controller/Controller.java
@@ -1,5 +1,8 @@
 package Controller;
 
+import Model.*;
+import java.util.ArrayList;
+
 public class Controller {
 
     public Controller() {
@@ -11,4 +14,50 @@ public class Controller {
         return id;
     }
     
+    public String getNamaKain(String id_kain){
+        String nama = "Kain ";
+        nama += getNamaBahan(id_kain) + " ";
+        nama += getNamaWarna(id_kain) + " ";
+        nama += getNamaMotif(id_kain);
+        return nama;
+    }
+    
+    public String getNamaBahan(String id_kain){
+        Sql sql = new Sql();
+        int id = Integer.parseInt(id_kain.split("-")[0]) - 1000;
+        String bahan = "";
+        ArrayList<BahanKain> listBahan = new ArrayList<>(sql.getAllBahan());
+        for(int i = 0; i < listBahan.size(); i++){
+            if(id == listBahan.get(i).getId_bahan()){
+                bahan = listBahan.get(i).getNama_bahan();
+            }
+        }
+        return bahan;
+    }
+    
+    public String getNamaWarna(String id_kain){
+        Sql sql = new Sql();
+        int id = Integer.parseInt(id_kain.split("-")[1]) - 2000;
+        String warna = "";
+        ArrayList<WarnaKain> listWarna = new ArrayList<>(sql.getAllWarna());
+        for(int i = 0; i < listWarna.size(); i++){
+            if(id == listWarna.get(i).getId_warna()){
+                warna = listWarna.get(i).getNama_warna();
+            }
+        }
+        return warna;
+    }
+    
+    public String getNamaMotif(String id_kain){
+        Sql sql = new Sql();
+        int id = Integer.parseInt(id_kain.split("-")[2]) - 3000;
+        String motif = "";
+        ArrayList<MotifKain> listMotif = new ArrayList<>(sql.getAllMotif());
+        for(int i = 0; i < listMotif.size(); i++){
+            if(id == listMotif.get(i).getId_motif()){
+                motif = listMotif.get(i).getNama_motif();
+            }
+        }
+        return motif;
+    }
 }
