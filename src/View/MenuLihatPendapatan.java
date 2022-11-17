81b59bce07a51c8e58e17234fc38c7516a312d93
JoeMaxwell <JoeMaxwell@Noir>
2022-11-17 18:25:54 +0700

    menyambungkan Menu Lihat Pendapatan dengan database (penambahan logic dsb)

--- src/View/MenuLihatPendapatan.java
+++ src/View/MenuLihatPendapatan.java
@@ -13,40 +13,46 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 
-
 /**
  *
  * @author JoeMaxwell
  */
 public class MenuLihatPendapatan {
-    
-    public MenuLihatPendapatan (){
+
+    public MenuLihatPendapatan() {
         JFrame frame = new JFrame("Register");
         frame.setSize(600, 475);
         frame.setLayout(null);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
-        
+
         JLabel totalPendapatanTitle = new JLabel("Total Pendapatan:");
         totalPendapatanTitle.setBounds(150, 75, 300, 50);
         totalPendapatanTitle.setFont(new Font("Sans", Font.CENTER_BASELINE, 35));
         frame.add(totalPendapatanTitle);
-               
-        JLabel totalPendapatan = new JLabel("Rp 120000");
+
+        Sql db = new Sql();
+        ArrayList<Transaksi> listTrans = db.getAllTransaksi();
+        int sumPendapatan = 0;
+        for (int i = 0; i < listTrans.size(); i++) {
+            sumPendapatan += listTrans.get(i).getTotal_bayar();
+        }
+
+        JLabel totalPendapatan = new JLabel("Rp " + sumPendapatan);
         totalPendapatan.setBounds(175, 150, 300, 50);
         totalPendapatan.setFont(new Font("Sans", Font.CENTER_BASELINE, 32));
-        frame.add(totalPendapatan);     
-         
+        frame.add(totalPendapatan);
+
         JLabel totalTransaksiTitle = new JLabel("Dari total: ");
         totalTransaksiTitle.setBounds(175, 250, 300, 50);
         totalTransaksiTitle.setFont(new Font("Sans", Font.CENTER_BASELINE, 20));
-        frame.add(totalTransaksiTitle);  
-        
-        JLabel totalTransaksi = new JLabel("12 transaksi");
+        frame.add(totalTransaksiTitle);
+
+        JLabel totalTransaksi = new JLabel(listTrans.size() + " transaksi");
         totalTransaksi.setBounds(175, 300, 300, 50);
         totalTransaksi.setFont(new Font("Sans", Font.CENTER_BASELINE, 20));
-        frame.add(totalTransaksi);  
-        
+        frame.add(totalTransaksi);
+
         JButton backMenu = new JButton("back to menu");
         backMenu.addActionListener(b -> {
             frame.dispose();
@@ -54,9 +60,9 @@ public class MenuLihatPendapatan {
         backMenu.setBounds(25, 375, 150, 40);
         frame.add(backMenu);
     }
-    
+
     public static void main(String[] args) {
         new MenuLihatPendapatan();
-        
+
     }
 }
